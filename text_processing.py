80e3fa55c89f4b448dc9c053607f4bd95eb398bf
cool-hammer <ajtwlstmdgks@gmail.com>
2021-01-19 15:32:14 +0900

    Update my code

--- text_processing.py
+++ text_processing.py
@@ -60,9 +60,7 @@ def no_vowels(input_string):
     """
     no_vowel_string = input_string
     
-    for vowel in 'aeiou':
-        no_vowel_string = no_vowel_string.replace(vowel, ' ')
-    
-    no_vowel_string = normalize(no_vowel_string)
+    for vowel in 'aeiouAEIOU':
+        no_vowel_string = no_vowel_string.replace(vowel, '')
     
     return no_vowel_string
